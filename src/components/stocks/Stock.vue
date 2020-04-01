93f30c551483451f143c2c3d1fe9b547fe5de6a9
Guilherme Peixe <guilherme.o.peixe@gmail.com>
2020-04-01 03:22:43 -0300

    Vuex e modulos do stock e portfólio adicionados

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,13 +2,13 @@
     <v-col class="pr-3 pb-3" lg="4" md="6" xs="12">
         <v-card color="green darken-3 white--text" :tile="true" >
             <v-card-title class="headline">
-                <strong>NOME <small>Price R$ 224</small></strong>
+                <strong> {{ stock.name }} <small>(Preço: {{ stock.price | toPrice }})</small></strong>
             </v-card-title>
         </v-card>
         <v-card :tile="true">
             <v-container fill-height width="600px">
-                <v-text-field label="Quantidade"/>
-                <v-btn class="ml-2" small tile color="green darken-3 white--text">Comprar</v-btn>
+                <v-text-field type="number" label="Quantidade" v-model.number="quantity"/>
+                <v-btn :disabled="quantity <= 0 || !Number.isInteger(quantity)" @click="buyStock" class="ml-2" small tile color="green darken-3 white--text">Comprar</v-btn>
             </v-container>
         </v-card>
     </v-col>
@@ -16,6 +16,30 @@
 
 <script>
 export default {
+    props: ['stock'],
+    data(){
+        return {
+            quantity: 0
+            }
+    },
+    methods: {
+        buyStock(){
+            const order = {
+                id: this.stock.id,
+                price: this.stock.price,
+                quantity: this.quantity
+            }
+
+            this.$store.dispatch('buyStock', order)
+            this.quantity = 0
+
+        }
+    },
+    filters: {
+        toPrice(price){
+            return `R$ ${price}`
+        }
+    }
 
 }
 </script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row  wrap>
-        <Stock v-for="stock in stocks" :key="stock.id"/>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
     </v-row>
 </template>
 
@@ -8,17 +8,11 @@
 import Stock from './Stock'
 export default {
     components: { Stock },
-    data(){
-        return {
-            stocks: [
-                { id: 1, name: 'BMW', price: 224},
-                { id: 2, name: 'Facebook', price: 132},
-                { id: 3, name: 'Google', price: 184},
-                { id: 4, name: 'Apple', price: 279},
-            ]
-        }
+    computed: {
+            stocks(){
+                return this.$store.getters.stocks
+            }
     }
-
 }
 </script>
 
